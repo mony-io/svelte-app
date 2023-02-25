0917850000a9460c8bfcda62b7c766d93986b8cb
mony-io <rysarakmony1666@gmailcom>
2023-02-25 09:01:52 +0700

    svelte-app

---
+++ src/components/FeedbackList.svelte
@@ -0,0 +1,20 @@
+<script>
+  // import {onMount, onDestroy} from 'svelte'
+  import { FeedbackStore } from "../stores";
+  import { fade, scale } from "svelte/transition";
+  import FeedbackItem from "./FeedbackItem.svelte";
+  // let feedback = []
+  // const unsubscribe = FeedbackStore.subscribe((data) => feedback = data)
+  // onMount(() => {
+  //   console.log('mounted')
+  // })
+  // onDestroy(() => {
+  //  unsubscribe()
+  // })
+</script>
+
+{#each $FeedbackStore as fb (fb.id)}
+  <div in:scale out:fade={{ duration: 500 }}>
+    <FeedbackItem item={fb} />
+  </div>
+{/each}
